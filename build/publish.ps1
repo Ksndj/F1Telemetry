f8a4f1c0675a98572bdd12275f2e90630c5ed06f
Ksndj <1067083140@qq.com>
2026-05-01 20:52:48 +0800

    chore: bump version to 1.5.0

--- build/publish.ps1
+++ build/publish.ps1
@@ -17,10 +17,10 @@ dotnet publish $projectPath `
     -m:1 `
     /p:PublishSingleFile=true `
     /p:IncludeNativeLibrariesForSelfExtract=true `
-    /p:Version=1.2.0 `
-    /p:AssemblyVersion=1.2.0.0 `
-    /p:FileVersion=1.2.0.0 `
-    /p:InformationalVersion=1.2.0 `
+    /p:Version=1.5.0 `
+    /p:AssemblyVersion=1.5.0.0 `
+    /p:FileVersion=1.5.0.0 `
+    /p:InformationalVersion=1.5.0 `
     -o $publishDir
 
 if ($LASTEXITCODE -ne 0) {
